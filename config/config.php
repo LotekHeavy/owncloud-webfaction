621331c8472f72e0e822bb770cce88dec65a833f
LotekHeavy <mail@heavy.ch>
2014-08-12 23:23:42 +0000

    added config.php, with an example to set a prefix for the url and disable the online app shop due some strange error messages, but should be fixed with new forthcoming version 7.0.2

---
+++ config/config.php
@@ -0,0 +1,27 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'xxxxxxxxxxxxxx',
+  'overwritewebroot' => '/cloud',
+  'passwordsalt' => 'xxxxxxxxxxxxxxxxxxx',
+  'trusted_domains' => 
+  array (
+    0 => 'www.heavy.ch',
+  ),
+  'datadirectory' => '/xxxxxxxxxxxxxx/var/data',
+  'dbtype' => 'mysql',
+  'version' => '7.0.1.1',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'xxxxxxxxxxx',
+  'installed' => true,
+  'mail_smtpmode' => 'sendmail',
+  'mail_smtpname' => 'xxxxxxxxx',
+  'mail_smtppassword' => 'xxxxxxxxxxxxxx',
+  'forcessl' => false,
+  'mail_from_address' => 'mail',
+  'mail_domain' => 'xxxxxxxxxxxxxxxx',
+  'appstoreenabled' => false,
+  'loglevel' => '2',
+);
